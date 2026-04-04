5b0167bbaac15eee72d9e71af257bced3abfee31
Mattia Stettler <mattia.stettler@ost.ch>
2026-04-04 20:02:53 +0200

    Finish new home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -16,24 +16,25 @@ function Home() {
                     <figure className="hero-content">
                         <div>
                             <Typewriter />
-                            <div className="flex items-center justify-center gap-2 mt-12 mb-12">
+                            <div className="flex items-center justify-center mt-14 mb-12">
                                 <img 
                                     src="/mattiastettler-small.png" 
                                     alt="Mattia" 
                                     className="w-16 h-16 rounded-full object-cover border-2 mr-4 border-primary"
                                 />
-                                <p className="text-3xl font-light">Ich bin</p>
-                                <p className="text-3xl font-light text-primary">Mattia</p>
-                                <p className="text-3xl font-light">Stettler</p>
+                                <p className="text-2xl sm:text-3xl font-light">Ich bin</p>
+                                <p className="text-2xl sm:text-3xl ml-2 mr-2 font-light text-secondary">Mattia</p>
+                                <p className="text-2xl sm:text-3xl font-light">Stettler</p>
+                                <p className="text-2xl sm:text-3xl font-light text-primary">;</p>
                             </div>
-                            <p className="text-2xl mt-6 mb-6">
-                                Hallo auf meiner Webseite, entdecke feine Rezepte und .
+                            <p className="text-xl mt-6 mb-6">
+                                Informatik Student und Product Engineer, verwöhne dich hier aber eigentlich lieber mit meinen Lieblingsrezepten als mit Pointer-Arithmetik, DNS und JavaScript.
                             </p>
                             <div className="flex justify-center gap-4">
-                                <Link to="/portfolio" className="btn btn-primary">
+                                <Link to="/portfolio" className="btn btn-primary hover:btn-secondary">
                                     Portfolio
                                 </Link>
-                                <Link to="/recipes" className="btn btn-accent">
+                                <Link to="/recipes" className="btn btn-primary hover:btn-secondary">
                                     Rezepte
                                 </Link>
                             </div>
